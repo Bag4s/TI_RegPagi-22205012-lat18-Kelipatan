89e1f4ce33b4b989be718a52bf0d91fd1fb67baa
Bag4s <149363597+Bag4s@users.noreply.github.com>
2023-10-30 14:42:27 +0700

    commit

---
+++ TI_RegPagi_22205012_Latihan18.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hello.bagas;
+
+/**
+ *
+ * @author ACER
+ */
+public class TI_RegPagi_22205012_Latihan18 {
+    public static void main(String[] args){
+    System.out.println("Bilangan kelipatan 3 dan 5 dari 3 sampai 35:");
+        for (double i = 3.5; i <= 35; i+=3.5){
+            System.out.println(i);
+        }
+    }
+}
